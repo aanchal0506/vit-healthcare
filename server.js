e3656e86fc1a0eb5bcad4c04ae0c3038907adbda
aanchal <ladhaanchal@gmail.com>
2025-04-16 12:37:23 +0530

    driver info in frontend

--- server.js
+++ server.js
@@ -245,7 +245,11 @@ app.post("/ambulances/book", async (req, res) => {
         availableAmbulance.pickupAddress = pickupAddress;
         await availableAmbulance.save();
 
-        res.status(200).json({ message: "Ambulance booked successfully", ambulance: availableAmbulance });
+        res.status(200).json({ message: "Ambulance booked successfully", driver: {
+            driverName: driver.driverName,
+            timeOfBooking: driver.timeOfBooking,
+            rtoNumber: driver.rtoNumber
+        }, ambulance: availableAmbulance });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
